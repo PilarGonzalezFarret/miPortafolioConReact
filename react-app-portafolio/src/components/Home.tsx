4824b44b5ed3bd2fe85a51783a22190549d72535
Pilar González-Farret <pilar.gonzalezfarret@gmail.com>
2025-01-26 20:43:04 -0300

    CSS y B
    Cambio colores, arreglo componentes, añado texto

--- react-app-portafolio/src/components/Home.tsx
+++ react-app-portafolio/src/components/Home.tsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
-
+import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Home pili's</h1>
+    <div  className={styles.home}>
       <h1>Pili's Portfolio!!</h1>
       <h4>Hi!! I'm happy toshare my portfolio with you!!</h4>
-      <p>Please feel free to browse through this app to learn more about my work and other interests.</p>
-      <Link to="/about">About</Link>
+      <br />
+      <p>Here, you'll find a showcase of my work, personal projects, and the things that inspire me most. Whether you're here to explore my professional journey, learn about my creative endeavors, or just get to know me better, I hope you find something that resonates with you.</p>
+      <br />
+      <p>Feel free to browse through this app and discover my skills, passions, and achievements. Don't hesitate to reach out if you'd like to collaborate, ask questions, or simply connect!</p>
+      <br />
+      <p>Thank you for stopping by—happy exploring!</p>
     </div>
   );
 }
--- react-app-portafolio/src/components/Navbar.tsx
+++ react-app-portafolio/src/components/Navbar.tsx
@@ -1,42 +1,31 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
-		return (
-			<div className={styles.navbar}>
-				<nav className="navbar navbar-expand-lg bg-body-tertiary">
-					<div className="container-fluid">
-						<a className="nav-link" href="#">
-							Home
-						</a>
-						<button
-						className="navbar-toggler"
-						type="button"
-						/* data-bs-toggle="collapse" */
-						data-bs-target="#navbarNavAltMarkup"
-						aria-controls="navbarNavAltMarkup"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-						>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-						<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-							<div className="navbar-nav">
-								<a className="nav-link" href="#">
-									About me
-								</a>
-								<a className="nav-link" href="#">
-									About this website
-								</a>
-								<a className="nav-link" href="#">
-									Contact me
-								</a>
-							</div>
-						</div>
-					</div>
-				</nav>
-			</div>
-		);
-	}
+  return (
+    <div className={styles.navbar} >
+      <nav className="navbar navbar-expand-lg" >
+        <div className="container-fluid" >
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+							<Link to="/about" className="nav-link" color="green">
+                About me
+              </Link>
+              <Link to="/about" className="nav-link">
+                About this website
+              </Link>
+              <Link to="/about" className="nav-link">
+                Contact me
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 export default Navbar;
