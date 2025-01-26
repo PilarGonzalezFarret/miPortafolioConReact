7b88e450a12d7e559767db4a342afd78bdefc387
Pilar González-Farret <pilar.gonzalezfarret@gmail.com>
2025-01-26 10:52:53 -0300

    Inserto Navbar
    Inserto Navbar con bootstrap, juego con algunos colores. Uso módulos de estilo.

--- react-app-portafolio/src/App.tsx
+++ react-app-portafolio/src/App.tsx
@@ -2,15 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
     
 
 function App() {
   return (
-    <div>
-      <h1>Pili's Portfolio!!</h1>
-      <h4>Hi!! I'm happy toshare my portfolio with you!!</h4>
-      <p>Please feel free to browse through this app to learn more about my work and other interests.</p>
-      
+    <div className='home'>
+      <Navbar></Navbar>
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
--- react-app-portafolio/src/components/Home.tsx
+++ react-app-portafolio/src/components/Home.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 
+
 function Home() {
   return (
     <div>
       <h1>Home pili's</h1>
+      <h1>Pili's Portfolio!!</h1>
+      <h4>Hi!! I'm happy toshare my portfolio with you!!</h4>
+      <p>Please feel free to browse through this app to learn more about my work and other interests.</p>
       <Link to="/about">About</Link>
     </div>
   );
